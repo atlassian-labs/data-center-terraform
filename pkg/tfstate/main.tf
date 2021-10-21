ed5ce456061afef14f7f5bebb35315bb307cf52d
Nasser Ghazali <36830944+nghazali@users.noreply.github.com>
2021-10-21 11:06:58 +1100

    TFKUBE-43 Restructured the project.   => (#5)
    Co-authored-by: nghazalibeiklar <nghazalibeiklar@atlassian.com>

--- pkg/tfstate/main.tf
+++ pkg/tfstate/main.tf
@@ -1,5 +1,5 @@
 # This file deals with where Terraform is keeping its state in AWS.
-# Please note that this file will run by `src/script/start-dc-terraform.sh' and no need to run manually.
+# Please note that this file will run by `pkg/script/start-dc-terraform.sh' and no need to run manually.
 provider "aws" {
   region = local.region
 }
