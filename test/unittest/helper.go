a583cb9d9f72c4337b39032dc49284316a572a7a
nghazalibeiklar <nghazalibeiklar@atlassian.com>
2021-10-22 16:55:01 +1100

    TFKUBE-34: Add some VPC unit tests

---
+++ test/unittest/helper.go
@@ -0,0 +1,29 @@
+package unittest
+
+import (
+	"testing"
+	"path/filepath"
+
+	"github.com/gruntwork-io/terratest/modules/aws"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
+)
+
+// Helper function
+func GenerateTFOptions(variables map[string]interface{}, t *testing.T) *terraform.Options {
+	exampleFolder := test_structure.CopyTerraformFolderToTemp(t, "../../pkg", "/modules/AWS/vpc")
+	awsRegion := aws.GetRandomStableRegion(t, nil, nil)
+	planFilePath := filepath.Join(exampleFolder, "plan.out")
+
+	tfOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: exampleFolder,
+		Vars:         variables,
+		EnvVars: map[string]string{
+			"AWS_DEFAULT_REGION": awsRegion,
+		},
+		PlanFilePath: planFilePath,
+	})
+
+	return tfOptions
+}
+
--- test/unittest/vpc_test.go
+++ test/unittest/vpc_test.go
@@ -1,43 +1,24 @@
 package unittest
 
 import (
-	"path/filepath"
 	"testing"
 
-	"github.com/gruntwork-io/terratest/modules/aws"
 	"github.com/gruntwork-io/terratest/modules/terraform"
-	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
 	"github.com/stretchr/testify/assert"
 )
 
 const TestResourceOwner = "terraform_unit_test"
 
-// An example of how to test the Terraform module in examples/terraform-aws-example using Terratest.
+Test CIDR variable.
 func TestVpcDefaultCidrBlock(t *testing.T) {
 	t.Parallel()
 
-	// Make a copy of the terraform module to a temporary directory. This allows running multiple tests in parallel
-	// against the same terraform module.
-	exampleFolder := test_structure.CopyTerraformFolderToTemp(t, "../../pkg", "/modules/AWS/vpc")
-
-	// Pick a random AWS region to test in. This helps ensure your code works in all regions.
-	// Given it's unit test, no resources are actually deployed, a random region will make sense.
-	awsRegion := aws.GetRandomStableRegion(t, nil, nil)
-
-	planFilePath := filepath.Join(exampleFolder, "plan.out")
-	tfOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-		TerraformDir: exampleFolder,
-		Vars: map[string]interface{}{
-			"vpc_name": "test-vpc",
-			"required_tags": map[string]interface{}{
-				"resource_owner": TestResourceOwner,
-			},
+	tfOptions := GenerateTFOptions(map[string]interface{}{
+		"vpc_name": "test-vpc",
+		"required_tags": map[string]interface{}{
+			"resource_owner": TestResourceOwner,
 		},
-		EnvVars: map[string]string{
-			"AWS_DEFAULT_REGION": awsRegion,
-		},
-		PlanFilePath: planFilePath,
-	})
+	}, t)
 
 	// Run `terraform init`, `terraform plan`, and `terraform show`
 	plan := terraform.InitAndPlanAndShowWithStruct(t, tfOptions)
@@ -53,25 +34,15 @@ func TestVpcDefaultCidrBlock(t *testing.T) {
 func TestVpcCustomisedCidrBlock(t *testing.T) {
 	t.Parallel()
 
-	exampleFolder := test_structure.CopyTerraformFolderToTemp(t, "../../pkg", "/modules/AWS/vpc")
-	awsRegion := aws.GetRandomStableRegion(t, nil, nil)
-	planFilePath := filepath.Join(exampleFolder, "plan.out")
-
-	tfOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-		TerraformDir: exampleFolder,
-		Vars: map[string]interface{}{
-			"vpc_name": "test-vpc",
-			"required_tags": map[string]interface{}{
-				"resource_owner": TestResourceOwner,
-			},
-			"vpc_cidr": "10.0.0.0/18",
+	tfOptions := GenerateTFOptions(map[string]interface{}{
+		"vpc_name": "test-vpc",
+		"required_tags": map[string]interface{}{
+			"resource_owner": TestResourceOwner,
 		},
-		EnvVars: map[string]string{
-			"AWS_DEFAULT_REGION": awsRegion,
-		},
-		PlanFilePath: planFilePath,
-	})
+		"vpc_cidr": "10.0.0.0/18",
+	}, t)
 
+	// Run `terraform init`, `terraform plan`, and `terraform show`
 	plan := terraform.InitAndPlanAndShowWithStruct(t, tfOptions)
 
 	terraform.RequirePlannedValuesMapKeyExists(t, plan, "module.vpc.aws_vpc.this[0]")
@@ -79,4 +50,80 @@ func TestVpcCustomisedCidrBlock(t *testing.T) {
 	vpcCidr := vpc.AttributeValues["cidr_block"]
 	assert.Equal(t, "10.0.0.0/18", vpcCidr)
 
+}
+
+func TestVpcInvalidCidrBlock(t *testing.T) {
+	t.Parallel()
+
+	tfOptions := GenerateTFOptions(map[string]interface{}{
+		"vpc_name": "test-vpc",
+		"required_tags": map[string]interface{}{
+			"resource_owner": TestResourceOwner,
+		},
+		"vpc_cidr": "10.0/16",
+	}, t)
+
+	_, error := terraform.InitAndPlanAndShowWithStructE(t, tfOptions)
+
+	assert.NotNil(t, error)
+	assert.Contains(t, error.Error(), "Invalid CIDR.")
+}
+
+// Test Enable DNS Hostname variable.
+func TestVpcDefaultDnsHostnamesBlock(t *testing.T) {
+	t.Parallel()
+
+	tfOptions := GenerateTFOptions(map[string]interface{}{
+		"vpc_name": "test-vpc",
+		"required_tags": map[string]interface{}{
+			"resource_owner": TestResourceOwner,
+		},
+	}, t)
+
+	// Run `terraform init`, `terraform plan`, and `terraform show`
+	plan := terraform.InitAndPlanAndShowWithStruct(t, tfOptions)
+
+	// Get the plan struct and assert values
+	terraform.RequirePlannedValuesMapKeyExists(t, plan, "module.vpc.aws_vpc.this[0]")
+	vpc := plan.ResourcePlannedValuesMap["module.vpc.aws_vpc.this[0]"]
+	DnsHostname := vpc.AttributeValues["enable_dns_hostnames"]
+	assert.Equal(t, true, DnsHostname)
+
+}
+
+func TestVpcCustomisedDnsHostnamesBlock(t *testing.T) {
+	t.Parallel()
+
+	tfOptions := GenerateTFOptions(map[string]interface{}{
+		"vpc_name": "test-vpc",
+		"required_tags": map[string]interface{}{
+			"resource_owner": TestResourceOwner,
+		},
+		"enable_dns_hostnames": false,
+	}, t)
+
+	// Run `terraform init`, `terraform plan`, and `terraform show`
+	plan := terraform.InitAndPlanAndShowWithStruct(t, tfOptions)
+
+	terraform.RequirePlannedValuesMapKeyExists(t, plan, "module.vpc.aws_vpc.this[0]")
+	vpc := plan.ResourcePlannedValuesMap["module.vpc.aws_vpc.this[0]"]
+	DnsHostname := vpc.AttributeValues["enable_dns_hostnames"]
+	assert.Equal(t, false, DnsHostname)
+
+}
+
+func TestVpcInvalidDnsHostnamesBlock(t *testing.T) {
+	t.Parallel()
+
+	tfOptions := GenerateTFOptions(map[string]interface{}{
+		"vpc_name": "test-vpc",
+		"required_tags": map[string]interface{}{
+			"resource_owner": TestResourceOwner,
+		},
+		"enable_dns_hostnames": "yes",
+	}, t)
+
+	_, error := terraform.InitAndPlanAndShowWithStructE(t, tfOptions)
+
+	assert.NotNil(t, error)
 }
