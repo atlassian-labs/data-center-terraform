cc9f67c6fa4e67cb2524f622e1de8262a05bf578
Nasser Ghazali <36830944+nghazali@users.noreply.github.com>
2021-10-26 16:01:16 +1100

    Update helper.go
    Rename instance to vpc_instance

--- test/unittest/helper.go
+++ test/unittest/helper.go
@@ -17,7 +17,7 @@ type PlanStruct struct {
 	plan terraform.PlanStruct
 }
 
-var instance *PlanStruct
+var vpc_instance *PlanStruct
 
 // Generates the VPC terraform plan for default values of the module
 // This is a singleton implementation for the plan
@@ -25,7 +25,7 @@ func GetVpcDefaultPlans(t *testing.T) *terraform.PlanStruct {
 
 	lock.Lock()
 	defer lock.Unlock()
-	if instance == nil {
+	if vpc_plan_instance == nil {
 		tfOptions := GenerateVpcTFOptions(map[string]interface{}{
 			"vpc_name": "test-vpc",
 			"vpc_tags": map[string]interface{}{
@@ -35,10 +35,10 @@ func GetVpcDefaultPlans(t *testing.T) *terraform.PlanStruct {
 
 		// Run `terraform init`, `terraform plan`, and `terraform show`
 		plan := terraform.InitAndPlanAndShowWithStruct(t, tfOptions)
-		instance = &PlanStruct{plan: *plan}
+		vpc_instance = &PlanStruct{plan: *plan}
 	}
 
-	return &instance.plan
+	return &vpc_instance.plan
 }
 
 func GenerateVpcTFOptions(variables map[string]interface{}, t *testing.T) *terraform.Options {
