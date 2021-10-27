f202e1a159e4e7c5439a8dac9e9f4fc3fe58c370
nghazalibeiklar <nghazalibeiklar@atlassian.com>
2021-10-27 12:01:44 +1100

    TFKUBE-64: Restructured the scripts and added more inline documentations

--- pkg/scripts/generate-tfstate-backend.sh
+++ pkg/scripts/generate-tfstate-backend.sh
@@ -1,22 +1,21 @@
+# This script will generate and override the `./pkg/tfstate/tfstate-locals.tf` and `./terraform-backend.tf`
+# to skip this part you can pass `-skip` as the parameter when you run this script
 
-root="$(pwd)"
-
-if [[ $1 == "-skip" ]]; then
-  BACKEND_TF=terraform-backend.tf
-  TFSTATE_LOCALS=pkg/tfstate/tfstate-locals.tf
-  if [[ -f $BACKEND_TF && -f $TFSTATE_LOCALS ]]; then
-    echo "Skipped generating tfstate variable files. The existing files will be used."
-    exit 0
-  fi
-    echo "Terraform state variable files are missing."
-    exit 1
+SCRIPT_PATH=$(dirname "$0")
+if [ ! $# -eq 2 ]; then
+      echo "The filename for terraform backend and tfstate local variable are missing."
+      exit 1
 fi
+
+BACKEND_TF="${1}"
+TFSTATE_LOCALS="${2}"
+
+LOCAL_TF="${SCRIPT_PATH}/../../locals.tf"
 
 echo "Generating the terraform state variables."
 # extract S3 bucket, dynamodb, tags, and region from locals.tf
-ENVIRONMENT_NAME=$(grep 'environment_name' locals.tf | sed -nE 's/^.*"(.*)".*$/\1/p')
-
-REGION=$(grep 'region' locals.tf | sed -nE 's/^.*"(.*)".*$/\1/p')
+ENVIRONMENT_NAME=$(grep 'environment_name' ${LOCAL_TF} | sed -nE 's/^.*"(.*)".*$/\1/p')
+REGION=$(grep 'region' ${LOCAL_TF} | sed -nE 's/^.*"(.*)".*$/\1/p')
 
 # Get the AWS account ID
 ACCOUNTID=$(aws sts get-caller-identity --query Account --output text)
@@ -27,15 +26,15 @@ DYNAMODB_TABLE="${ENVIRONMENT_NAME}_${PRODUCT}_${ACCOUNTID}"
 BUCKET_KEY="${ENVIRONMENT_NAME}-${PRODUCT}-${ACCOUNTID}"
 
 # Generate the terraform backend, where terraform store the state of the infrastructure
-sed 's/<REGION>/'${REGION}'/g'  ./pkg/templates/terraform-backend.tf.tmpl | \
+sed 's/<REGION>/'${REGION}'/g'  "${SCRIPT_PATH}/../templates/terraform-backend.tf.tmpl" | \
 sed  's/<BUCKET_NAME>/'${S3_BUCKET}'/g' | \
 sed 's/<BUCKET_KEY>/'${BUCKET_KEY}'/g'  | \
 sed 's/<DYNAMODB_TABLE>/'${DYNAMODB_TABLE}'/g' \
-  > terraform-backend.tf
+  > ${BACKEND_TF}
 
 # Generate the locals for terraform state
 sed 's/<REGION>/'${REGION}'/g'  ./pkg/templates/tfstate-locals.tf.tmpl | \
 sed  's/<BUCKET_NAME>/'${S3_BUCKET}'/g' | \
 sed 's/<BUCKET_KEY>/'${BUCKET_KEY}'/g'  | \
 sed 's/<DYNAMODB_TABLE>/'${DYNAMODB_TABLE}'/g' \
-  > ./pkg/tfstate/tfstate-locals.tf
+  > ${TFSTATE_LOCALS}
--- pkg/scripts/start-dc-terraform.sh
+++ pkg/scripts/start-dc-terraform.sh
@@ -1,50 +1,90 @@
 #!/usr/bin/env bash
+# This script manages to deploy the infrastructure for the given product
+#
+# Usage:  ./start-dc-terraform.sh <product> [-skip]
+# <product>: - At this the script supports only 'bamboo'. If the arguments are missing 'bamboo' will consider by default
+# [-skip]: '-skip' as the second argument will skip re-generating the terraform-backend.tf and tfstate-local.tf files
 
 set -e
-root="$(pwd)"
+PRODUCT=$1
+SKIP_GENERATE_VARS=$2
+CURRENT_PATH="$(pwd)"
+SCRIPT_PATH="$(dirname "$0")"
 
-if [ $# -eq 0 ]
-  then
-    PRODUCT="bamboo"
-else
-  # for now only bamboo is supported
-  declare -l PRODUCT
-  PRODUCT=$1
-  if [ $PRODUCT != "bamboo" ]; then
-    echo "'$1' is not supported."
-    exit 1
+process_arguments() {
+  if [ $# -eq 0 ]
+    then
+      PRODUCT="bamboo"
+  else
+    # for now only bamboo is supported
+    declare -l PRODUCT
+    if [ ${PRODUCT} != "bamboo" ]; then
+      echo "'${PRODUCT}' is not supported."
+      exit 1
+    fi
   fi
-fi
+}
 
-# This script will generate the `./pkg/tfstate/tfstate-locals.tf` and `./terraform-backend.tf`
-# to skip this part you can pass `-skip` as the second parameter when you run this script
-source "./pkg/scripts/generate-tfstate-backend.sh" ${2}
-# fetch the locals.tf file from terraform project
-cp -fr locals.tf ./pkg/tfstate
-cd "$root/pkg/tfstate"
+generate_backend_variables() {
+  BACKEND_TF="${SCRIPT_PATH}/../../terraform-backend.tf"
+  TFSTATE_LOCALS="${SCRIPT_PATH}/../tfstate/tfstate-locals.tf"
+  if [[ $SKIP_GENERATE_VARS == "-skip" ]]; then
+    if [[ -f $BACKEND_TF && -f $TFSTATE_LOCALS ]]; then
+      echo "Skipped generating tfstate variable files. The existing files will be used."
+      exit 0
+    fi
+      echo "Terraform state backend/variable files are missing."
+      exit 1
+  fi
 
-# Check if the S3 bucket is existed otherwise create the bucket to keep the terraform state
-echo "Checking the terraform state."
-set +e
-aws s3api head-bucket --bucket "$S3_BUCKET"
-S3_BUCKET_EXISTS=$?
-set -e
-if [ $S3_BUCKET_EXISTS -eq 0 ]
-then
-  echo "S3 bucket '$S3_BUCKET' is already existed."
-  cd "$root"
-else
-  # create s3 bucket to be used for keep state of the terraform project
-  echo "Creating '$S3_BUCKET' bucket for storing the terraform state..."
+  source "${SCRIPT_PATH}/generate-tfstate-backend.sh" ${BACKEND_TF} ${TFSTATE_LOCALS}
+
+  # fetch the locals.tf file from terraform project
+  cp -fr "${SCRIPT_PATH}/../../locals.tf" "${SCRIPT_PATH}/../tfstate"
+}
+
+create_tfstate_bucket() {
+  # Check if the S3 bucket is existed otherwise create the bucket to keep the terraform state
+  echo "Checking the terraform state."
+  cd "${SCRIPT_PATH}/../tfstate"
+  set +e
+  aws s3api head-bucket --bucket "$S3_BUCKET"
+  S3_BUCKET_EXISTS=$?
+  set -e
+  if [ $S3_BUCKET_EXISTS -eq 0 ]
+  then
+    echo "S3 bucket '$S3_BUCKET' is already existed."
+    cd "${CURRENT_PATH}"
+  else
+    # create s3 bucket to be used for keep state of the terraform project
+    echo "Creating '$S3_BUCKET' bucket for storing the terraform state..."
+    terraform init
+    terraform apply -auto-approve
+    sleep 5s
+
+    echo "Migrating the terraform state to S3 bucket..."
+    cd "${CURRENT_PATH}"
+    terraform init -migrate-state
+  fi
+}
+
+create_infrastructure() {
+  Echo "Starting to analyze the infrastructure..."
   terraform init
   terraform apply -auto-approve
-  sleep 5s
+}
 
-  echo "Migrating the terraform state to S3 bucket..."
-  cd "$root"
-  terraform init -migrate-state
-fi
+# Process the arguments - Validate the PRODUCT (first argument) and second argument to see if skip generating backend vars
+process_arguments
 
-Echo "Starting to analyze the infrastructure..."
-terraform init
-terraform apply -auto-approve
+# Generates ./terraform-backend.tf and ./pkg/tfstate/tfstate-local.tf using the content of local.tf and current aws account
+generate_backend_variables
+
+# Create S3 bucket and dynamodb table to keep state and manage lock if they are not created yet
+create_tfstate_bucket
+
+# Deploy the infrastructure if is not created yet otherwise apply the changes to existing infrastructure
+create_infrastructure
+
+
+
