e3805fbbdfe43fd416230c5f186dcb23931ed311
Jun <jjeong@atlassian.com>
2021-11-02 12:34:41 +1100

    TFKUBE-62: VPC TEST added

--- test/e2etest/bamboo_test.go
+++ test/e2etest/bamboo_test.go
@@ -4,8 +4,9 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/gruntwork-io/terratest/modules/helm"
+	"github.com/gruntwork-io/terratest/modules/aws"
 	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestBambooModule(t *testing.T) {
@@ -17,17 +18,23 @@ func TestBambooModule(t *testing.T) {
 	testConfig := GenerateConfig(product, awsRegion)
 	tfOptions := GenerateTerraformOptions(testConfig.terraformConfig, t)
 	// kubectlOptions := GenerateKubectlOptions(testConfig.kubectlConfig)
-	helmOptions := GenerateHelmOptions(testConfig.helmConfig)
+	// helmOptions := GenerateHelmOptions(testConfig.helmConfig)
 
 	// Clean up. NOTE: defer is LIFO stack so the order is important.
 	defer terraform.Destroy(t, tfOptions)
-	defer helm.RemoveRepo(t, helmOptions, "atlassian-data-center")
-	defer helm.Delete(t, helmOptions, testConfig.releaseName, true)
+	// defer helm.RemoveRepo(t, helmOptions, "atlassian-data-center")
+	// defer helm.Delete(t, helmOptions, testConfig.releaseName, true)
 
 	terraform.InitAndApply(t, tfOptions)
 
-	helm.AddRepo(t, helmOptions, "atlassian-data-center", "https://atlassian.github.io/data-center-helm-charts")
+	// helm.AddRepo(t, helmOptions, "atlassian-data-center", "https://atlassian.github.io/data-center-helm-charts")
 
-	helm.Install(t, helmOptions, fmt.Sprintf("atlassian-data-center/%s", product), testConfig.releaseName)
+	// helm.Install(t, helmOptions, fmt.Sprintf("atlassian-data-center/%s", product), testConfig.releaseName)
+	vpcId := terraform.Output(t, tfOptions, "vpc_id")
+	vpc := aws.GetVpcById(t, vpcId, awsRegion)
 
+	fmt.Println(vpc, "this is vpc")
+	assert.Equal(t, vpc.Name, "atlassian-dc-e2e-test-bamboo-vpc")
+	assert.Equal(t, vpc.Tags["product"], "bamboo")
+	assert.Len(t, vpc.Subnets, 4)
 }
