1c882236a8e9b57aa816ca814ec72f509e9687c2
nghazalibeiklar <nghazalibeiklar@atlassian.com>
2021-11-04 14:33:29 +1100

    TFKUBE-72: Added tags

--- pkg/products/common/locals.tf
+++ pkg/products/common/locals.tf
@@ -1,4 +1,7 @@
 locals {
+  required_tags = {
+  }
+
   vpc_name     = format("atlassian-dc-%s-vpc", var.environment_name)
   cluster_name = format("atlassian-dc-%s-cluster", var.environment_name)
 }
--- pkg/products/common/main.tf
+++ pkg/products/common/main.tf
@@ -3,5 +3,5 @@ module "vpc" {
   source = "../../modules/AWS/vpc"
 
   vpc_name = local.vpc_name
-  vpc_tags = var.resource_tags
+  vpc_tags = merge(var.resource_tags, local.required_tags)
 }
