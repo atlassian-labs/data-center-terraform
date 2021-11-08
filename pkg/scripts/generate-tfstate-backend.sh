ceb2b421e3ca346fdc91e23f5fe5439f85705166
nghazalibeiklar <nghazalibeiklar@atlassian.com>
2021-11-08 18:11:49 +1100

    Refined the S3_BUCKET name

--- pkg/scripts/generate-tfstate-backend.sh
+++ pkg/scripts/generate-tfstate-backend.sh
@@ -22,7 +22,7 @@ REGION=$(grep 'region' ${CONFIG_FILE} | sed -nE 's/^.*"(.*)".*$/\1/p')
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 
 # Generates the unique s3 bucket and key names for the deployment for keeping the terraform state
-S3_BUCKET="atlassian-data-center-terraform-state-${REGION}-${AWS_ACCOUNT_ID}"
+S3_BUCKET="atlassian-dc-${REGION}-${AWS_ACCOUNT_ID}-terraform-state"
 BUCKET_KEY="${ENVIRONMENT_NAME}-${AWS_ACCOUNT_ID}"
 
 # length of the bucket name should be less than 64 characters
