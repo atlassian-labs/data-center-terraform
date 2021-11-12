d80a96f1fa1626cdaefd91b84ae5fd10f642970f
Jun Jeong <85263332+jjeongatl@users.noreply.github.com>
2021-11-12 15:38:40 +1100

    TFKUBE-68:E2E GitHub action (#27)
    * TFKUBE-68: github action for e2e test init
    
    * TFKUBE-68: run upload artifact even test fails.
    
    * TFKUBE-68: helper should provide all variables to run e2e
    
    * TFKUBE-68: checkout version updated
    
    * TFKUBE-68: provider config update
    
    * TFKUBE-68: e2e test config update
    
    * TFKUBE-68: temp github action branch removed
    
    * TFKUBE68: k8s provider load config removed
    
    * TFKUBE-68:install aws-iam-authenticator
    
    * TFKUBE-68: removed version info
    
    * TFKUBE-68: replace install authenticator to manual
    
    * TFKUBE-68: load config using eks kubeconfig
    
    * Testing aws-iam-authentication
    
    * Install aws-iam-auth in working dir
    
    * export path of aws-iam-auth before running e2e
    
    * TFKUBE-68: Enable test
    
    * TFKUBE-68: testing env_vars
    
    * TFKUBE-68: testing fix
    
    * TFKUBE-68: this works?
    
    * TFKUBE-68: removing path
    
    * TFKUBE-68: last try
    
    * TFKUBE-68: revert experiment

--- pkg/modules/AWS/eks/main.tf
+++ pkg/modules/AWS/eks/main.tf
@@ -39,10 +39,3 @@ data "aws_eks_cluster" "cluster" {
 data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_id
 }
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  config_path            = "~/.kube/config"
-}
--- pkg/modules/AWS/eks/providers.tf
+++ pkg/modules/AWS/eks/providers.tf
@@ -3,3 +3,10 @@ provider "helm" {
     config_path = module.eks.kubeconfig_filename
   }
 }
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  config_path            = module.eks.kubeconfig_filename
+}
