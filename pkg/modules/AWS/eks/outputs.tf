df2ce3e363f101ca8f55b2c070b3a8ce807fb663
Jun <jjeong@atlassian.com>
2021-11-12 15:05:09 +1100

    TFKUBE-75: unittest fix

--- pkg/modules/AWS/eks/outputs.tf
+++ pkg/modules/AWS/eks/outputs.tf
@@ -29,4 +29,5 @@ output "kubernetes_provider_config" {
     token                  = data.aws_eks_cluster_auth.cluster.token
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   }
+  sensitive = true
 }
