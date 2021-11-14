9400b7409658a47e8205b9b941cbfd19a8a79ecf
Jun Jeong <85263332+jjeongatl@users.noreply.github.com>
2021-11-15 10:25:21 +1100

    Fix merge mistake

--- pkg/modules/AWS/eks/providers.tf
+++ pkg/modules/AWS/eks/providers.tf
@@ -19,10 +19,3 @@ provider "helm" {
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   }
 }
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  config_path            = module.eks.kubeconfig_filename
-}
