162e409f81f8609e3ff29e135b64c74ca59206c9
nghazalibeiklar <nghazalibeiklar@atlassian.com>
2021-11-15 17:28:53 +1100

    TFKUBE-108: Fixed a typo in vpc cidr validation message

--- pkg/modules/AWS/vpc/variables.tf
+++ pkg/modules/AWS/vpc/variables.tf
@@ -13,7 +13,7 @@ variable "vpc_cidr" {
   default     = "10.0.0.0/18"
   validation {
     condition     = can(regex("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])/([1-9]|1[0-9]|2[0-4])$", var.vpc_cidr))
-    error_message = "Invalid CIDR. Valid format is '<IPv4>/[8-24]' e.g: 10.0.0.0/18."
+    error_message = "Invalid CIDR. Valid format is '<IPv4>/[1-24]' e.g: 10.0.0.0/18."
   }
 }
 
