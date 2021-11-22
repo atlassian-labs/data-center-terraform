e4fc61fe99563b4e3babd3bf61d8e2754148ebba
Nasser Ghazali <36830944+nghazali@users.noreply.github.com>
2021-11-22 12:35:52 +1100

    TFKUBE-118: Added db into helm release (#41)
    * TFKUBE-118: Added db into helm release and set the priority to fix destroy problem. Added namespace to db secret that was missing before
    
    * Refactored the db secret
    
    * Added jdbc connection string in output and also added documentation to explain how to decode db secret
    
    Co-authored-by: nghazalibeiklar <nghazalibeiklar@atlassian.com>

--- pkg/products/bamboo/outputs.tf
+++ pkg/products/bamboo/outputs.tf
@@ -31,10 +31,14 @@ output "rds_instance_id" {
   value = module.database.rds_instance_id
 }
 
+output "rds_jdbc_connection" {
+  value = module.database.rds_jdbc_connection
+}
+
 output "db_name" {
   value = module.database.rds_db_name
 }
 
-output "kubernetes_db_secret_name" {
-  value = module.database.kubernetes_rds_secret_name
+output "kubernetes_rds_secret_name" {
+  value = kubernetes_secret.rds_secret.metadata[0].name
 }
