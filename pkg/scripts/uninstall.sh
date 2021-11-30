80a4d2282aea75656dad8f14b44a6f4453e0963d
Jun <jjeong@atlassian.com>
2021-12-01 00:32:19 +1100

    TFKUBE-109:test fix

--- pkg/scripts/uninstall.sh
+++ pkg/scripts/uninstall.sh
@@ -158,6 +158,7 @@ cleanup_asg_ec2_tagging_module() {
   rm -rf .terraform
   rm -rf .terraform.lock.hcl
   rm -rf terraform.tfstate
+  rm -rf terraform.tfstate.backkup
 }
 
 # Process the arguments
