2642c3dc3b7db74adb2f0efe7ac731628be89992
nghazalibeiklar <nghazalibeiklar@atlassian.com>
2021-12-10 16:32:06 +1100

    Added a comment

--- pkg/modules/AWS/eks/locals.tf
+++ pkg/modules/AWS/eks/locals.tf
@@ -1,4 +1,6 @@
 
 locals {
+  # When the framework get uninstalled, there is no appNode attribute for node_group and so next install will break
+  # because eks  is still is not created but terraform tries to evaluate the cluster_asg_name
   cluster_asg_name = try(module.eks.node_groups.appNodes.resources[0].autoscaling_groups[0].name, null)
 }
