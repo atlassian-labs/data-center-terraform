4e8a08ded9807628dd444640cea74fd958e363b8
Nasser Ghazali <36830944+nghazali@users.noreply.github.com>
2021-12-20 09:51:35 +1100

    TFKUBE-182: Fix e2e test (#90)
    * TFKUBE-182: Copy variables.tf to asg_ec2_tagging folder before running tests.
    
    * Created a dummy config file in asg_ec2_tagging folder
    
    * Created a dummy locals file in asg_ec2_tagging folder
    
    * Renamed the cluster, vpc, and config file name to match to the latest code change
    
    Co-authored-by: Yifei Zhang <yzhang@atlassian.com>
    Co-authored-by: nghazalibeiklar <nghazalibeiklar@atlassian.com>

--- test/e2etest/bamboo_test.go
+++ test/e2etest/bamboo_test.go
@@ -61,19 +61,19 @@ func TestBambooModule(t *testing.T) {
 
 func assertVPC(t *testing.T, awsRegion string, vpcOutput VpcOutput, environmentConfig EnvironmentConfig) {
 	vpc := aws.GetVpcById(t, vpcOutput.Id, awsRegion)
-	resourceTags := environmentConfig.TerraformConfig.Variables["resource_tags"].(map[string]interface{})
+	resourceTags := environmentConfig.TerraformConfig.Variables["resource_tags"].(map[string]string)
 
-	assert.Equal(t, fmt.Sprintf("atlassian-dc-%s-vpc", environmentConfig.EnvironmentName), vpc.Name)
+	assert.Equal(t, fmt.Sprintf("atlas-%s-vpc", environmentConfig.EnvironmentName), vpc.Name)
 	assert.Equal(t, resourceTags["resource_owner"], vpc.Tags["resource_owner"])
 	assert.Len(t, vpc.Subnets, 4)
 }
 
 func assertEKS(t *testing.T, awsRegion string, vpcOutput VpcOutput, environmentConfig EnvironmentConfig) {
-	resourceTags := environmentConfig.TerraformConfig.Variables["resource_tags"].(map[string]interface{})
+	resourceTags := environmentConfig.TerraformConfig.Variables["resource_tags"].(map[string]string)
 	session := GenerateAwsSession(awsRegion)
 	eksClient := eks.New(session)
 	describeClusterInput := &eks.DescribeClusterInput{
-		Name: awsSdk.String(fmt.Sprintf("atlassian-dc-%s-cluster", environmentConfig.EnvironmentName)),
+		Name: awsSdk.String(fmt.Sprintf("atlas-%s-cluster", environmentConfig.EnvironmentName)),
 	}
 
 	eksInfo, err := eksClient.DescribeCluster(describeClusterInput)
