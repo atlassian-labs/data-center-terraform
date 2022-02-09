2c62ada621db9d8fcb8d6a559d0ac84ff535dd43
Adam Brokes <abrokes@atlassian.com>
2022-02-09 14:22:18 +1100

    TFKUBE-270: Move the helm release to `helm.tf` file

---
+++ modules/products/confluence/helm.tf
@@ -0,0 +1,63 @@
+# Install helm chart for confluence Data Center.
+resource "helm_release" "confluence" {
+  name       = local.product_name
+  namespace  = var.namespace
+  repository = local.helm_chart_repository
+  chart      = local.confluence_helm_chart_name
+  version    = local.confluence_helm_chart_version
+  timeout    = 40 * 60 # dataset import can take a long time
+
+  values = [
+    yamlencode({
+      confluence = {
+        clustering = {
+          enabled = true
+        }
+        resources = {
+          jvm = {
+            maxHeap = local.confluence_software_resources.maxHeap
+            minHeap = local.confluence_software_resources.minHeap
+          }
+          container = {
+            requests = {
+              cpu    = local.confluence_software_resources.cpu
+              memory = local.confluence_software_resources.mem
+            }
+          }
+        }
+      }
+      database = {
+        type = "postgresql"
+        url  = module.database.rds_jdbc_connection
+        credentials = {
+          secretName = kubernetes_secret.rds_secret.metadata[0].name
+        }
+      }
+      volumes = {
+        localHome = {
+          persistentVolumeClaim = {
+            create = true
+          }
+        }
+        sharedHome = {
+          customVolume = {
+            persistentVolumeClaim = {
+              claimName = var.pvc_claim_name
+            }
+          }
+          subPath = local.product_name
+        }
+      }
+    }),
+    local.ingress_settings,
+    local.license_settings,
+  ]
+}
+
+data "kubernetes_service" "confluence" {
+  depends_on = [helm_release.confluence]
+  metadata {
+    name      = local.product_name
+    namespace = var.namespace
+  }
+}
