de718512b221ec7882a153aa6bed2b53994f1504
Adam Brokes <abrokes@atlassian.com>
2022-02-11 16:47:55 +1100

    TFKUBE-270: Remove ignored unit test

--- test/unittest/confluence_test.go
+++ test/unittest/confluence_test.go
@@ -1,7 +1,6 @@
 package unittest
 
 import (
-	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
 
@@ -21,34 +20,6 @@ func TestConfluenceVariablesPopulatedWithValidValues(t *testing.T) {
 	assert.Equal(t, "deployed", confluence.AttributeValues["status"])
 	assert.Equal(t, "confluence", confluence.AttributeValues["chart"])
 	assert.Equal(t, "https://atlassian.github.io/data-center-helm-charts", confluence.AttributeValues["repository"])
-}
-
-func TestConfluenceSynchronyInstalled(t *testing.T) {
-	t.Parallel()
-	t.SkipNow()
-
-	datasetVars := ConfluenceCorrectVariables
-	datasetVars["enable_synchrony"] = true
-	datasetVars["ingress"] = map[string]interface{}{
-		"enabled": true,
-	}
-	tfOptions := GenerateTFOptions(datasetVars, t, "products/confluence")
-	plan := terraform.InitAndPlanAndShowWithStruct(t, tfOptions)
-
-	// verify Dataset import job
-	jobKey := "kubernetes_job.import_dataset[0]"
-	terraform.RequirePlannedValuesMapKeyExists(t, plan, jobKey)
-	job := plan.ResourcePlannedValuesMap[jobKey]
-	assert.Equal(t, "kubernetes_job", job.Type)
-
-	spec := job.AttributeValues["spec"].([]interface{})[0]
-	template := spec.(map[string]interface{})["template"].([]interface{})[0]
-	jobSpec := template.(map[string]interface{})["spec"].([]interface{})[0]
-	container := jobSpec.(map[string]interface{})["container"].([]interface{})[0]
-	commands := container.(map[string]interface{})["command"].([]interface{})
-
-	// we need to download the dataset
-	assert.Contains(t, commands, fmt.Sprintf("apk update && apk add wget && wget %s -O /shared-home/dataset_to_import.zip", DatasetUrl))
 }
 
 // Variables
