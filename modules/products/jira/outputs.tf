ebea7e41f3527a72f76b3df55c11c4d5c0f3f9d8
Yifei Zhang <yzhang@atlassian.com>
2022-02-15 13:58:17 +1100

    TFKUBE-279: Refactor - cleaned up unused outputs

--- modules/products/jira/outputs.tf
+++ modules/products/jira/outputs.tf
@@ -1,27 +1,3 @@
-output "vpc_id" {
-  value       = var.vpc.vpc_id
-  description = "VPC Id"
-}
-output "private_subnets" {
-  value       = var.vpc.private_subnets
-  description = "VPC private subnets"
-}
-
-output "private_subnets_cidr_blocks" {
-  value       = var.vpc.private_subnets_cidr_blocks
-  description = "VPC private subnet CIDR blocks"
-}
-
-output "public_subnets" {
-  value       = var.vpc.public_subnets
-  description = "VPC public subnets"
-}
-
-output "public_subnets_cidr_blocks" {
-  value       = var.vpc.public_subnets_cidr_blocks
-  description = "VPC public subnet CIDR blocks"
-}
-
 output "product_domain_name" {
   value = local.use_domain ? "https://${local.product_domain_name}" : "http://${data.kubernetes_service.jira.status[0].load_balancer[0].ingress[0].hostname}"
 }
