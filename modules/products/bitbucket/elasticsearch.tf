359a2fe1e023fd43b41fb6c4670555288c2eca63
nghazalibeiklar <nghazalibeiklar@atlassian.com>
2022-02-17 17:42:36 +1100

    Made elasticsearch installation depend on external elasticsearch

--- modules/products/bitbucket/elasticsearch.tf
+++ modules/products/bitbucket/elasticsearch.tf
@@ -1,6 +1,8 @@
 # Create the elasticsearch based on Elasticsearch Helm charts (https://github.com/elastic/helm-charts/tree/main/elasticsearch)
 
 resource "helm_release" "elasticsearch" {
+  count = var.elasticsearch_endpoint == null ? 1 : 0
+
   name       = "${local.elasticsearch_name}-${var.environment_name}"
   namespace  = var.namespace
   repository = local.elasticsearch_helm_chart_repository
