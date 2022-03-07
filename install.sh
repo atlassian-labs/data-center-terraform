991565bcb244b0c3855e437bead5223178646089
Dylan Rathbone <drathbone@atlassian.com>
2022-03-07 13:30:03 +1000

    TFKUBE-285: Updates in accordance with PR feedback

--- install.sh
+++ install.sh
@@ -275,6 +275,7 @@ set_synchrony_url() {
 
 # Update the current load balancer listener on port 7999 to use the TCP protocol
 enable_ssh_tcp_protocol_on_lb_listener() {
+  readonly SSH_TCP_PORT="7999"
   local install_bitbucket
   local region
   local load_balancer_dns
@@ -287,9 +288,9 @@ enable_ssh_tcp_protocol_on_lb_listener() {
     region=$(get_variable 'region' "${CONFIG_ABS_PATH}")
     load_balancer_dns=$(terraform output | grep '"load_balancer_hostname" =' | sed -nE 's/^.*"(.*)".*$/\1/p')
     load_balancer_name=$(echo "${load_balancer_dns}" | cut -d '-' -f 1)
-    original_instance_port=$(aws elb describe-load-balancers --load-balancer-name ${load_balancer_name} --query 'LoadBalancerDescriptions[*].ListenerDescriptions[?Listener.LoadBalancerPort==`7999`].Listener[].InstancePort | [0]' --region "${region}")
+    original_instance_port=$(aws elb describe-load-balancers --load-balancer-name ${load_balancer_name} --query 'LoadBalancerDescriptions[*].ListenerDescriptions[?Listener.LoadBalancerPort==`'"${SSH_TCP_PORT}"'`].Listener[].InstancePort | [0]' --region "${region}")
 
-    log "Enabling SSH connectivity for Bitbucket. Updating load balancer [${load_balancer_dns}] listener protocol from HTTP to TCP on port 7999..."
+    log "Enabling SSH connectivity for Bitbucket. Updating load balancer [${load_balancer_dns}] listener protocol from HTTP to TCP on port ${SSH_TCP_PORT}..."
     describe_lb_listener "${load_balancer_name}" "${region}"
 
     # delete the current listener for port 7999 and re-create but using the TCP protocol instead
@@ -297,7 +298,7 @@ enable_ssh_tcp_protocol_on_lb_listener() {
       log "Load balancer listener protocol updated for ${load_balancer_dns}."
       describe_lb_listener "${load_balancer_name}" "${region}"
     else
-      log "ERROR! There was an issue updating the load balancer [${load_balancer_dns}] listener protocol from HTTP to TCP on port 7999. You may want to do this manually via the AWS Console."
+      log "ERROR! There was an issue updating the load balancer [${load_balancer_dns}] listener protocol from HTTP to TCP on port ${SSH_TCP_PORT}. You may want to do this manually via the AWS Console."
     fi
   fi
 }
