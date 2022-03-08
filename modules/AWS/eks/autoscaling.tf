c034573225b3cdd205215759a5f5232c9aea14c9
Adam Brokes <abrokes@atlassian.com>
2022-03-08 17:31:41 +1100

    Remove redundant `cluster` from role name
    Co-authored-by: Nasser Ghazali <36830944+nghazali@users.noreply.github.com>

--- modules/AWS/eks/autoscaling.tf
+++ modules/AWS/eks/autoscaling.tf
@@ -3,7 +3,7 @@ module "autoscaler_iam_role" {
   version = "4.13.2"
 
   create_role  = true
-  role_name    = "${var.cluster_name}-cluster-autoscaler"
+  role_name    = "${var.cluster_name}-autoscaler"
   provider_url = replace(module.eks.cluster_oidc_issuer_url, "https://", "")
 
   role_policy_arns = [
