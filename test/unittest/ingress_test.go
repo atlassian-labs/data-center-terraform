56efb69eacaa329d12baa6e58d35eae90b0d968d
Dylan Rathbone <drathbone@atlassian.com>
2022-03-13 09:30:29 +1000

    TFKUBE-369: Unit tests

--- test/unittest/ingress_test.go
+++ test/unittest/ingress_test.go
@@ -9,11 +9,12 @@ import (
 
 var ingressModule = "AWS/ingress"
 
-func TestIngressIsCreated(t *testing.T) {
+func TestIngressIsCreatedWithDomain(t *testing.T) {
 	t.Parallel()
 
 	tfOptions := GenerateTFOptions(map[string]interface{}{
 		"ingress_domain": "test.deplops.com",
+		"enable_ssh_tcp": true,
 	}, t, ingressModule)
 
 	plan := terraform.InitAndPlanAndShowWithStruct(t, tfOptions)
@@ -21,7 +22,7 @@ func TestIngressIsCreated(t *testing.T) {
 	// verify the input variable
 	assert.Equal(t, "test.deplops.com", plan.RawPlan.Variables["ingress_domain"].Value)
 
-	// verify ingress
+	// verify ingress is created
 	ingressKey := "helm_release.ingress"
 	terraform.RequirePlannedValuesMapKeyExists(t, plan, ingressKey)
 	ingress := plan.ResourcePlannedValuesMap[ingressKey]
@@ -29,27 +30,41 @@ func TestIngressIsCreated(t *testing.T) {
 	assert.Equal(t, "ingress-nginx", ingress.AttributeValues["chart"])
 	assert.Equal(t, "https://kubernetes.github.io/ingress-nginx", ingress.AttributeValues["repository"])
 
-	// verify certificate
+	// verify certificate is created
 	certificateKey := "module.ingress_certificate[0].aws_acm_certificate.this[0]"
 	certificate := plan.ResourcePlannedValuesMap[certificateKey]
 	assert.Equal(t, "*.test.deplops.com", certificate.AttributeValues["domain_name"])
 	assert.Contains(t, certificate.AttributeValues["subject_alternative_names"], "test.deplops.com")
 	assert.Equal(t, "DNS", certificate.AttributeValues["validation_method"])
 
-	// verify DNS records
+	// verify DNS records are created
 	route53Key := "aws_route53_zone.ingress[0]"
 	route53 := plan.ResourcePlannedValuesMap[route53Key]
 	assert.Equal(t, "test.deplops.com", route53.AttributeValues["name"])
 }
 
-func TestIngressRequireDomainVariable(t *testing.T) {
+func TestIngressIsCreatedWithoutDomain(t *testing.T) {
 	t.Parallel()
 
 	tfOptions := GenerateTFOptions(map[string]interface{}{}, t, ingressModule)
 
-	_, err := terraform.InitAndPlanAndShowWithStructE(t, tfOptions)
+	plan := terraform.InitAndPlanAndShowWithStruct(t, tfOptions)
 
-	assert.Error(t, err)
-	assert.Contains(t, err.Error(), "No value for required variable")
-	assert.Contains(t, err.Error(), "\"ingress_domain\" is not set")
+	// verify ingress is created
+	ingressKey := "helm_release.ingress"
+	terraform.RequirePlannedValuesMapKeyExists(t, plan, ingressKey)
+	ingress := plan.ResourcePlannedValuesMap[ingressKey]
+	assert.Equal(t, "deployed", ingress.AttributeValues["status"])
+	assert.Equal(t, "ingress-nginx", ingress.AttributeValues["chart"])
+	assert.Equal(t, "https://kubernetes.github.io/ingress-nginx", ingress.AttributeValues["repository"])
+
+	// verify certificate is not created
+	certificateKey := "module.ingress_certificate[0].aws_acm_certificate.this[0]"
+	certificate := plan.ResourcePlannedValuesMap[certificateKey]
+	assert.Nil(t, certificate)
+
+	// verify DNS records are not created
+	route53Key := "aws_route53_zone.ingress[0]"
+	route53 := plan.ResourcePlannedValuesMap[route53Key]
+	assert.Nil(t, route53)
 }
