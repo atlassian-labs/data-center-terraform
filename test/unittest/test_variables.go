30d855904ee9dbc126b15e9de633c7fc04cc1fa0
nghazalibeiklar <nghazalibeiklar@atlassian.com>
2022-04-10 18:47:34 -0700

    fixed unittests

--- test/unittest/test_variables.go
+++ test/unittest/test_variables.go
@@ -166,11 +166,11 @@ const dbVersion = 13
 const invalidInputRdsInstanceId = "1-"
 
 var DbValidVariable = map[string]interface{}{
-	"product":           inputProduct,
-	"rds_instance_id":   inputRdsInstanceId,
-	"instance_class":    inputInstanceClass,
-	"allocated_storage": inputAllocatedStorage,
-	"iops":              inputIops,
+	"product":                 inputProduct,
+	"rds_instance_identifier": inputRdsInstanceId,
+	"instance_class":          inputInstanceClass,
+	"allocated_storage":       inputAllocatedStorage,
+	"iops":                    inputIops,
 	"eks": map[string]interface{}{
 		"kubernetes_provider_config": map[string]interface{}{
 			"host":                   "dummy-host",
