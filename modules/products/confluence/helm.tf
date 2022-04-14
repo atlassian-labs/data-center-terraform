c54c49ada68611eccc9c33c797fe32d3dd72154b
Jun <jjeong@atlassian.com>
2022-04-14 12:09:10 +1000

    TFKUBE-405: increase helm timeout

--- modules/products/confluence/helm.tf
+++ modules/products/confluence/helm.tf
@@ -8,7 +8,7 @@ resource "helm_release" "confluence" {
   repository = local.helm_chart_repository
   chart      = local.confluence_helm_chart_name
   version    = local.confluence_helm_chart_version
-  timeout    = 10 * 60 # autoscaler potentially needs to scale up the cluster
+  timeout    = 15 * 60 # autoscaler potentially needs to scale up the cluster
 
   values = [
     yamlencode({
