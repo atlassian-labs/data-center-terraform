b6fdb7686ceca8d117f0acccf1ce1b7d606f6a7c
Nasser Ghazali <36830944+nghazali@users.noreply.github.com>
2022-04-21 16:28:30 -0700

    Update modules/AWS/nfs/outputs.tf
    Co-authored-by: D∇L∆N <drathbone@atlassian.com>

--- modules/AWS/nfs/outputs.tf
+++ modules/AWS/nfs/outputs.tf
@@ -2,6 +2,6 @@ output "helm_release_nfs_service_ip" {
   value = data.kubernetes_service.nfs.spec[0].cluster_ip
 }
 
-output "nfs_pvc_claim_name" {
+output "nfs_claim_name" {
   value = var.shared_home_size == null ? null : kubernetes_persistent_volume_claim.shared-home-pvc[0].metadata[0].name
 }
