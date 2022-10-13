bde8615af0ea7a6e8f79f82b46c9b30b6c1222e3
yivantsov-atlassian <52448429+bianchi2@users.noreply.github.com>
2022-10-13 15:14:29 +1100

    Collect k8s logs and cleanup AWS resources (#265)
    * Collect k8s logs and cleanup AWS resources
    
    * Delete unused volumes
    
    Co-authored-by: Yevhen Ivantsov <yivantsov@atlassian.com>

--- scripts/collect_k8s_logs.sh
+++ scripts/collect_k8s_logs.sh
@@ -3,6 +3,7 @@ set -e
 
 CLUSTER_NAME=$1
 AWS_REGION=$2
+DEBUG_FOLDER=$3
 
 SCRIPT=$(basename "$0")
 
@@ -22,36 +23,43 @@ fi
 echo "[INFO]: Updating kubeconfig context"
 aws eks update-kubeconfig --name ${CLUSTER_NAME} --region ${AWS_REGION}
 
-DEBUG_FOLDER="$(pwd)/debug-$1-$2"
-mkdir -p ${DEBUG_FOLDER}
+if [ -z "${DEBUG_FOLDER}" ]; then
+  DEBUG_FOLDER="../test/e2etest/artifacts/k8s-debug-$1-$2"
+fi
+
+mkdir -p "${DEBUG_FOLDER}"
 
 echo "[INFO]: Getting pods logs"
 
 PODS=$(kubectl get pods -n atlassian --no-headers -o custom-columns=":metadata.name")
 for POD in ${PODS[@]}; do
-  kubectl logs ${POD} -n atlassian > ${DEBUG_FOLDER}/${POD}_log.log 2>&1
-  kubectl describe pod ${POD} -n atlassian > ${DEBUG_FOLDER}/${POD}_describe.log 2>&1
+  kubectl logs ${POD} -n atlassian > "${DEBUG_FOLDER}"/${POD}_log.log 2>&1
+  kubectl describe pod ${POD} -n atlassian > "${DEBUG_FOLDER}"/${POD}_describe.log 2>&1
 done
 
 NGINX_PODS=$(kubectl get pods -n ingress-nginx --no-headers -o custom-columns=":metadata.name")
 for POD in ${NGINX_PODS[@]}; do
-  kubectl logs ${POD} -n ingress-nginx > ${DEBUG_FOLDER}/${POD}_log.log 2>&1
-  kubectl describe pod ${POD} -n ingress-nginx > ${DEBUG_FOLDER}/${POD}_describe.log 2>&1
+  kubectl logs ${POD} -n ingress-nginx > "${DEBUG_FOLDER}"/${POD}_log.log 2>&1
+  kubectl describe pod ${POD} -n ingress-nginx > "${DEBUG_FOLDER}"/${POD}_describe.log 2>&1
 done
 
-kubectl describe svc -n ingress-nginx > ${DEBUG_FOLDER}/nginx_svc_describe.log 2>&1
+# checking status of Nginx ingress is important to troubleshoot any LOadBalancer issues
+kubectl describe svc -n ingress-nginx > "${DEBUG_FOLDER}"/nginx_svc_describe.log 2>&1
 
 echo "[INFO]: Getting namespaces pods and events"
 
-kubectl get events -n atlassian > ${DEBUG_FOLDER}/events.log 2>&1
-kubectl get pods -n atlassian > ${DEBUG_FOLDER}/all_pods.log 2>&1
+kubectl get events -n atlassian > "${DEBUG_FOLDER}"/events.log 2>&1
+kubectl get pods -n atlassian > "${DEBUG_FOLDER}"/all_pods.log 2>&1
 
 echo "[INFO]: Describing resources"
 
 RESOURCES=(svc ingress pvc pv)
 for RESOURCE in ${RESOURCES[@]}; do
-  kubectl describe ${RESOURCE} -n atlassian > ${DEBUG_FOLDER}/${RESOURCE}_describe.log 2>&1
+  kubectl describe ${RESOURCE} -n atlassian > "${DEBUG_FOLDER}"/${RESOURCE}_describe.log 2>&1
 done
 
+echo "[INFO]: Describing nodes"
+kubectl describe nodes > "${DEBUG_FOLDER}"/nodes.log 2>&1
+
 echo "[INFO]: Logs and events saved to ${DEBUG_FOLDER}"
-ls -la ${DEBUG_FOLDER}
+ls -la "${DEBUG_FOLDER}"
