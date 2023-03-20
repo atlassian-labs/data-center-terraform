9e84527c166e2725340e3972168bd880f90ec99e
yivantsov-atlassian <52448429+bianchi2@users.noreply.github.com>
2023-03-21 10:51:13 +1100

    Update eks output (#316)
    * Update eks output
    
    * Rename output
    
    * Mark asg name as non sensitive in outputs
    
    * Remove asg name from outputs
    
    ---------
    
    Co-authored-by: Yevhen Ivantsov <yivantsov@atlassian.com>

--- modules/AWS/eks/locals.tf
+++ modules/AWS/eks/locals.tf
@@ -1,8 +1,5 @@
 
 locals {
-  # When the framework get uninstalled, there is no appNode attribute for node_group and so next install will break
-  # because eks is still is not created but terraform tries to evaluate the cluster_asg_name
-  cluster_asg_name = try(module.eks.node_groups.appNodes.resources[0].autoscaling_groups[0].name, null)
 
   autoscaler_service_account_namespace = "kube-system"
   autoscaler_service_account_name      = "cluster-autoscaler-aws-cluster-autoscaler-chart"
