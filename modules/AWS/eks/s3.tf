437a84703766ac966752961af2f264ce2d56bd39
Dylan Rathbone <drathbone@atlassian.com>
2023-04-24 09:39:59 +1000

    Add aws_s3_bucket_ownership_controls resource

--- modules/AWS/eks/s3.tf
+++ modules/AWS/eks/s3.tf
@@ -4,7 +4,16 @@ resource "aws_s3_bucket" "confluence_storage_bucket" {
   force_destroy = true
 }
 
+resource "aws_s3_bucket_ownership_controls" "confluence_s3" {
+  count       = var.confluence_s3_attachments_storage ? 1 : 0
+  bucket = aws_s3_bucket.confluence_storage_bucket[count.index].id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
 resource "aws_s3_bucket_acl" "confluence_storage_acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.confluence_s3]
   count       = var.confluence_s3_attachments_storage ? 1 : 0
   bucket = aws_s3_bucket.confluence_storage_bucket[count.index].id
   acl    = "private"
