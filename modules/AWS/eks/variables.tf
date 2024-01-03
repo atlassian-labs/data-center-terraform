75e59351f78439f2e218aa0c770d47ea38a481c3
Yevhen Ivantsov <yivantsov@atlassian.com>
2024-01-03 12:37:03 +1100

    Upgrade k8s version

--- modules/AWS/eks/variables.tf
+++ modules/AWS/eks/variables.tf
@@ -11,8 +11,8 @@ variable "eks_version" {
   description = "EKS K8s version"
   type        = number
   validation {
-    condition     = can(regex("^1\\.2[7-8]", var.eks_version))
-    error_message = "Invalid EKS K8S version. Valid versions are from 1.27 to 1.28."
+    condition     = can(regex("^1\\.2[7-9]", var.eks_version))
+    error_message = "Invalid EKS K8S version. Valid versions are from 1.27 to 1.29."
   }
 }
 
