d7a1475b20e3f39ae078ce42e58a79f7632efab7
yivantsov-atlassian <52448429+bianchi2@users.noreply.github.com>
2024-04-30 07:33:24 +1000

    Improve dcapt-snapshots tests (#387)
    * Update snapshot tests
    
    * Linting errors
    
    ---------
    
    Co-authored-by: Yevhen Ivantsov <yivantsov@atlassian.com>

--- test/unittest/snapshots_test.go
+++ test/unittest/snapshots_test.go
@@ -1,27 +1,46 @@
 package unittest
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/gruntwork-io/terratest/modules/aws"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	testStructure "github.com/gruntwork-io/terratest/modules/test-structure"
 	"github.com/stretchr/testify/assert"
+	"io"
+	"net/http"
+	"os"
 	"path/filepath"
 	"testing"
 )
 
+type SnapshotDetails struct {
+	UsEast2 string `json:"us-east-2"`
+	UsEast1 string `json:"us-east-1"`
+}
+
+type Data struct {
+	Type      string            `json:"type"`
+	Size      string            `json:"size"`
+	Snapshots []SnapshotDetails `json:"snapshots"`
+}
+
+type Version struct {
+	Version     string `json:"version"`
+	Data        []Data `json:"data"`
+	BuildNumber string `json:"build_number"`
+}
+
+type Product struct {
+	Versions []Version `json:"versions"`
+}
+
 type DCSnapshots struct {
-	JiraEbs            string
-	JiraEbsLocal       string
-	JiraRds            string
-	JsmEbs             string
-	JsmRds             string
-	ConfluenceEbs      string
-	ConfluenceEbsLocal string
-	ConfluenceRds      string
-	BitbucketEbs       string
-	BitbucketRds       string
-	CrowdEbs           string
-	CrowdRds           string
+	Jira       Product `json:"jira"`
+	Jsm        Product `json:"jsm"`
+	Confluence Product `json:"confluence"`
+	Bitbucket  Product `json:"bitbucket"`
+	Crowd      Product `json:"crowd"`
 }
 
 var vars = map[string]interface{}{
@@ -59,14 +78,77 @@ var vars = map[string]interface{}{
 	"bamboo_dataset_url":            "https://centaurus-datasets.s3.amazonaws.com/bamboo/dcapt-bamboo.zip",
 }
 
+func getLargeRdsSnapshot(product Product) *Data {
+	if len(product.Versions) > 0 {
+		firstVersion := product.Versions[0]
+		for _, data := range firstVersion.Data {
+			if data.Size == "large" && data.Type == "rds" {
+				return &data
+			}
+		}
+	}
+	return nil
+}
+
+func getLargeEbsSnapshot(product Product) *Data {
+	if len(product.Versions) > 0 {
+		firstVersion := product.Versions[0]
+		for _, data := range firstVersion.Data {
+			if data.Size == "large" && data.Type == "ebs" {
+				return &data
+			}
+		}
+	}
+	return nil
+}
+
+func getLargeLocalHomeSnapshot(product Product) *Data {
+	if len(product.Versions) > 0 {
+		firstVersion := product.Versions[0]
+		for _, data := range firstVersion.Data {
+			if data.Size == "large" && data.Type == "local-home" {
+				return &data
+			}
+		}
+	}
+	return nil
+}
+
+func fetchSnapshotsJson() (*DCSnapshots, error) {
+	url := "https://raw.githubusercontent.com/atlassian/dc-app-performance-toolkit/master/app/util/k8s/dcapt-snapshots.json"
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch data: %v", err)
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read response body: %v", err)
+	}
+
+	f, err := os.Create("../dcapt-snapshots.json")
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	_, err = f.Write(body)
+	if err != nil {
+		return nil, err
+	}
+
+	var snapshots DCSnapshots
+	if err := json.Unmarshal(body, &snapshots); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal JSON: %v", err)
+	}
+	return &snapshots, nil
+}
+
 func TestJsmSnapshots(t *testing.T) {
 	t.Parallel()
-	jiraSnapshots := DCSnapshots{
-		JsmEbs: "snap-0098dceccb1e60b46",
-		JsmRds: "arn:aws:rds:us-east-2:585036043680:snapshot:dcapt-jsm-5-12-4",
-	}
+	dcSnapshots, err := fetchSnapshotsJson()
+	assert.NoError(t, err)
 	vars["jira_image_repository"] = "atlassian/jira-servicemanagement"
-	vars["jira_version_tag"] = "5.12.4"
+	vars["jira_version_tag"] = dcSnapshots.Jsm.Versions[0].Version
 	exampleFolder := testStructure.CopyTerraformFolderToTemp(t, "../..", "")
 	awsRegion := aws.GetRandomStableRegion(t, nil, nil)
 	planFilePath := filepath.Join(exampleFolder, "plan.out")
@@ -84,26 +166,15 @@ func TestJsmSnapshots(t *testing.T) {
 	assert.NotNil(t, jsmRdsSnapshot)
 	jsmEbsVolumeSnapshot := plan.ResourcePlannedValuesMap["module.nfs[0].aws_ebs_volume.shared_home"].AttributeValues["snapshot_id"]
 	assert.NotNil(t, jsmEbsVolumeSnapshot)
-	assert.Equal(t, jiraSnapshots.JsmRds, jsmRdsSnapshot)
-	assert.Equal(t, jiraSnapshots.JsmEbs, jsmEbsVolumeSnapshot)
+	assert.Equal(t, getLargeRdsSnapshot(dcSnapshots.Jsm).Snapshots[0].UsEast2, jsmRdsSnapshot)
+	assert.Equal(t, getLargeEbsSnapshot(dcSnapshots.Jsm).Snapshots[0].UsEast2, jsmEbsVolumeSnapshot)
 }
+
 func TestSnapshotsFromJson(t *testing.T) {
 	t.Parallel()
 
-	// rather than parse the ../dcapt-snapshots.json, snap ids are copied from it
-	dcSnapshots := DCSnapshots{
-		JiraEbs:            "snap-0800247b9bad8a16d",
-		JiraEbsLocal:       "snap-01942e6924d6094d3",
-		JiraRds:            "arn:aws:rds:us-east-2:585036043680:snapshot:dcapt-jira-9-12-4",
-		ConfluenceEbs:      "snap-00a8fab739b46f2b7",
-		ConfluenceEbsLocal: "snap-051ca1f3060f748a9",
-		ConfluenceRds:      "arn:aws:rds:us-east-2:585036043680:snapshot:dcapt-confluence-7-19-19",
-		BitbucketEbs:       "snap-019e03768c88ea9d2",
-		BitbucketRds:       "arn:aws:rds:us-east-2:585036043680:snapshot:dcapt-bitbucket-7-21-22",
-		CrowdEbs:           "snap-0824995529fb96ba3",
-		CrowdRds:           "arn:aws:rds:us-east-2:585036043680:snapshot:dcapt-crowd-5-2-3",
-	}
-
+	dcSnapshots, err := fetchSnapshotsJson()
+	assert.NoError(t, err)
 	vars := vars
 
 	// a bit of copy-paste as we can't use GenerateTFOptions as is (we need to run terraform plan in the root of the directory)
@@ -132,23 +203,28 @@ func TestSnapshotsFromJson(t *testing.T) {
 	assert.NotNil(t, bitbucketPreInstallJob)
 	assert.NotNil(t, jiraPreInstallJob)
 
-	// assert that snapshot IDs are in outputs, i.e. they have been correctly extracted from
-	// ../dcapt-snapshots.json. Additionally, Confluence and Crowd have build_numbers
-	assert.Equal(t, dcSnapshots.BitbucketEbs, plan.RawPlan.PlannedValues.Outputs["bitbucket_ebs_snapshot"].Value)
-	assert.Equal(t, dcSnapshots.BitbucketRds, plan.RawPlan.PlannedValues.Outputs["bitbucket_rds_snapshot"].Value)
+	// assert that snapshot IDs are in outputs, i.e. they have been correctly extracted from ../dcapt-snapshots.json
+	// Additionally, Confluence and Crowd have build_numbers
 
-	assert.Equal(t, dcSnapshots.JiraEbs, plan.RawPlan.PlannedValues.Outputs["jira_ebs_snapshot"].Value)
-	assert.Equal(t, dcSnapshots.JiraEbsLocal, plan.RawPlan.PlannedValues.Outputs["jira_local_home_snapshot"].Value)
-	assert.Equal(t, dcSnapshots.JiraRds, plan.RawPlan.PlannedValues.Outputs["jira_rds_snapshot"].Value)
+	// Bitbucket tests
+	assert.Equal(t, getLargeEbsSnapshot(dcSnapshots.Bitbucket).Snapshots[0].UsEast2, plan.RawPlan.PlannedValues.Outputs["bitbucket_ebs_snapshot"].Value)
+	assert.Equal(t, getLargeRdsSnapshot(dcSnapshots.Bitbucket).Snapshots[0].UsEast2, plan.RawPlan.PlannedValues.Outputs["bitbucket_rds_snapshot"].Value)
 
-	assert.Equal(t, dcSnapshots.ConfluenceEbs, plan.RawPlan.PlannedValues.Outputs["confluence_ebs_snapshot"].Value)
-	assert.Equal(t, dcSnapshots.ConfluenceEbsLocal, plan.RawPlan.PlannedValues.Outputs["confluence_local_home_snapshot"].Value)
-	assert.Equal(t, dcSnapshots.ConfluenceRds, plan.RawPlan.PlannedValues.Outputs["confluence_rds_snapshot"].Value)
-	assert.Equal(t, "8804", plan.RawPlan.PlannedValues.Outputs["confluence_db_snapshot_build_number"].Value)
+	// Jira tests
+	assert.Equal(t, getLargeLocalHomeSnapshot(dcSnapshots.Jira).Snapshots[0].UsEast2, plan.RawPlan.PlannedValues.Outputs["jira_local_home_snapshot"].Value)
+	assert.Equal(t, getLargeEbsSnapshot(dcSnapshots.Jira).Snapshots[0].UsEast2, plan.RawPlan.PlannedValues.Outputs["jira_ebs_snapshot"].Value)
+	assert.Equal(t, getLargeRdsSnapshot(dcSnapshots.Jira).Snapshots[0].UsEast2, plan.RawPlan.PlannedValues.Outputs["jira_rds_snapshot"].Value)
 
-	assert.Equal(t, dcSnapshots.CrowdEbs, plan.RawPlan.PlannedValues.Outputs["crowd_ebs_snapshot"].Value)
-	assert.Equal(t, dcSnapshots.CrowdRds, plan.RawPlan.PlannedValues.Outputs["crowd_rds_snapshot"].Value)
-	assert.Equal(t, "1944", plan.RawPlan.PlannedValues.Outputs["crowd_db_snapshot_build_number"].Value)
+	// Confluence tests
+	assert.Equal(t, getLargeLocalHomeSnapshot(dcSnapshots.Confluence).Snapshots[0].UsEast2, plan.RawPlan.PlannedValues.Outputs["confluence_local_home_snapshot"].Value)
+	assert.Equal(t, getLargeEbsSnapshot(dcSnapshots.Confluence).Snapshots[0].UsEast2, plan.RawPlan.PlannedValues.Outputs["confluence_ebs_snapshot"].Value)
+	assert.Equal(t, getLargeRdsSnapshot(dcSnapshots.Confluence).Snapshots[0].UsEast2, plan.RawPlan.PlannedValues.Outputs["confluence_rds_snapshot"].Value)
+	assert.Equal(t, dcSnapshots.Confluence.Versions[0].BuildNumber, plan.RawPlan.PlannedValues.Outputs["confluence_db_snapshot_build_number"].Value)
+
+	// Crowd tests
+	assert.Equal(t, getLargeEbsSnapshot(dcSnapshots.Crowd).Snapshots[0].UsEast2, plan.RawPlan.PlannedValues.Outputs["crowd_ebs_snapshot"].Value)
+	assert.Equal(t, getLargeRdsSnapshot(dcSnapshots.Crowd).Snapshots[0].UsEast2, plan.RawPlan.PlannedValues.Outputs["crowd_rds_snapshot"].Value)
+	assert.Equal(t, dcSnapshots.Crowd.Versions[0].BuildNumber, plan.RawPlan.PlannedValues.Outputs["crowd_db_snapshot_build_number"].Value)
 
 	// assert that the right RDS snapshots are in the right database modules
 	jiraRdsSnapshot := plan.ResourcePlannedValuesMap["module.database[0].module.db.module.db_instance.aws_db_instance.this[0]"].AttributeValues["snapshot_identifier"]
@@ -156,10 +232,10 @@ func TestSnapshotsFromJson(t *testing.T) {
 	bitbucketRdsSnapshot := plan.ResourcePlannedValuesMap["module.database[2].module.db.module.db_instance.aws_db_instance.this[0]"].AttributeValues["snapshot_identifier"]
 	crowdRdsSnapshot := plan.ResourcePlannedValuesMap["module.database[3].module.db.module.db_instance.aws_db_instance.this[0]"].AttributeValues["snapshot_identifier"]
 
-	assert.Equal(t, dcSnapshots.ConfluenceRds, confluenceRdsSnapshot)
-	assert.Equal(t, dcSnapshots.BitbucketRds, bitbucketRdsSnapshot)
-	assert.Equal(t, dcSnapshots.CrowdRds, crowdRdsSnapshot)
-	assert.Equal(t, dcSnapshots.JiraRds, jiraRdsSnapshot)
+	assert.Equal(t, getLargeRdsSnapshot(dcSnapshots.Confluence).Snapshots[0].UsEast2, confluenceRdsSnapshot)
+	assert.Equal(t, getLargeRdsSnapshot(dcSnapshots.Bitbucket).Snapshots[0].UsEast2, bitbucketRdsSnapshot)
+	assert.Equal(t, getLargeRdsSnapshot(dcSnapshots.Crowd).Snapshots[0].UsEast2, crowdRdsSnapshot)
+	assert.Equal(t, getLargeRdsSnapshot(dcSnapshots.Jira).Snapshots[0].UsEast2, jiraRdsSnapshot)
 
 	// assert ebs and local home snapshots are in ebs_volume aws resources
 	jiraEbsVolumeSnapshot := plan.ResourcePlannedValuesMap["module.nfs[0].aws_ebs_volume.shared_home"].AttributeValues["snapshot_id"]
@@ -170,11 +246,12 @@ func TestSnapshotsFromJson(t *testing.T) {
 	jiraEbsLocalSnapshot := plan.ResourcePlannedValuesMap["module.jira[0].aws_ebs_volume.local_home[0]"].AttributeValues["snapshot_id"]
 	confluenceEbsLocalSnapshot := plan.ResourcePlannedValuesMap["module.confluence[0].aws_ebs_volume.local_home[0]"].AttributeValues["snapshot_id"]
 
-	assert.Equal(t, dcSnapshots.BitbucketEbs, bitbucketEbsVolumeSnapshot)
-	assert.Equal(t, dcSnapshots.JiraEbs, jiraEbsVolumeSnapshot)
-	assert.Equal(t, dcSnapshots.ConfluenceEbs, confluenceEbsVolumeSnapshot)
-	assert.Equal(t, dcSnapshots.CrowdEbs, crowdEbsVolumeSnapshot)
+	assert.Equal(t, getLargeEbsSnapshot(dcSnapshots.Bitbucket).Snapshots[0].UsEast2, bitbucketEbsVolumeSnapshot)
+	assert.Equal(t, getLargeEbsSnapshot(dcSnapshots.Jira).Snapshots[0].UsEast2, jiraEbsVolumeSnapshot)
+	assert.Equal(t, getLargeEbsSnapshot(dcSnapshots.Confluence).Snapshots[0].UsEast2, confluenceEbsVolumeSnapshot)
+	assert.Equal(t, getLargeEbsSnapshot(dcSnapshots.Crowd).Snapshots[0].UsEast2, crowdEbsVolumeSnapshot)
 
-	assert.Equal(t, dcSnapshots.ConfluenceEbsLocal, confluenceEbsLocalSnapshot)
-	assert.Equal(t, dcSnapshots.JiraEbsLocal, jiraEbsLocalSnapshot)
+	// assert local home snap ids
+	assert.Equal(t, getLargeLocalHomeSnapshot(dcSnapshots.Confluence).Snapshots[0].UsEast2, confluenceEbsLocalSnapshot)
+	assert.Equal(t, getLargeLocalHomeSnapshot(dcSnapshots.Jira).Snapshots[0].UsEast2, jiraEbsLocalSnapshot)
 }
