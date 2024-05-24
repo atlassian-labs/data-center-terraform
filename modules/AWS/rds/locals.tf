b4c4971744213e3cca39bbb9581f73ccfaeea499
Alex Metelytsia <ometelytsia@atlassian.com>
2024-05-24 23:41:09 +0200

    Update aws rds locals.tf with latest db engine versions (#412)

--- modules/AWS/rds/locals.tf
+++ modules/AWS/rds/locals.tf
@@ -3,23 +3,25 @@ locals {
   create_random_password = var.db_master_password == null ? true : false
   db_jdbc_connection     = "jdbc:postgresql://${module.db.db_instance_endpoint}/${var.product}"
 
-  # RDS major version is mapped to the lastest minor version for more details.
+  # RDS major version is mapped to the latest minor version for more details.
   # https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_PostgreSQL.html#PostgreSQL.Concepts.General.version13
   engine_version = lookup(
     {
-      "11" = "11.14",
-      "12" = "12.16",
-      "13" = "13.13",
-      "14" = "14.11"
-  }, var.major_engine_version, "11.14")
+      "12" = "12.19",
+      "13" = "13.15",
+      "14" = "14.12",
+      "15" = "15.7",
+      "16" = "16.3",
+  }, var.major_engine_version, "14.12")
 
   family = lookup(
     {
-      "11" = "postgres11",
       "12" = "postgres12",
       "13" = "postgres13",
-      "14" = "postgres14"
-  }, var.major_engine_version, "postgres11")
+      "14" = "postgres14",
+      "15" = "postgres15",
+      "16" = "postgres16",
+  }, var.major_engine_version, "postgres14")
 
   db_snapshot_engine_version       = var.snapshot_identifier != null ? data.aws_db_snapshot.atlassian_db_snapshot[0].engine_version : null
   db_snapshot_major_engine_version = var.snapshot_identifier != null ? element(split(".", data.aws_db_snapshot.atlassian_db_snapshot[0].engine_version), 0) : null
