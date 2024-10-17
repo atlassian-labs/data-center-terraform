4f7a1f494b0cbf621218f680ab26b6390953e821
yivan-atl <52448429+bianchi2@users.noreply.github.com>
2024-10-17 10:15:34 +0700

    Manually scale down Confluence/Jira statefulsets (#435)
    * Scale down sts before terraform destroys PVCs
    
    * Fix label selector to get confluence sts too
    
    * Add comment to function
    
    ---------
    
    Co-authored-by: Yevhen Ivantsov <yivantsov@atlassian.com>

---
+++ scripts/scale.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+source "./scripts/common.sh"
+
+NAMESPACE="atlassian"
+TIMEOUT=120
+INTERVAL=5
+PRODUCT=$1
+DESIRED_REPLICAS=$2
+
+# check if this is the initial release and exit
+STS=$(kubectl get sts -l=app.kubernetes.io/name="${PRODUCT}" -n "${NAMESPACE}" -o jsonpath='{.items[*].metadata.name}')
+if [ -z "$STS" ]; then
+  log "No StatefulSets found"
+  exit 0
+fi
+
+# get existing sts replicas
+STS_REPLICAS=$(kubectl get sts $STS -n "${NAMESPACE}" -ojsonpath='{.spec.replicas}')
+if [ -z "${STS_REPLICAS}" ]; then
+  log "Failed to get StatefulSets replicas"
+  exit 0
+fi
+# Check if DESIRED_REPLICAS is less than STS_REPLICAS and manually scale down
+# before Terraform attempts destroying local-home PVC, PV and EBS vol
+if [ "${DESIRED_REPLICAS}" -lt "${STS_REPLICAS}" ]; then
+  log "Scaling down ${PRODUCT} StatefulSet to ${DESIRED_REPLICAS} replicas"
+  kubectl scale sts "${PRODUCT}" -n "${NAMESPACE}" --replicas="${DESIRED_REPLICAS}"
+
+  # make sure pods are gone
+  START_TIME=$(date +%s)
+  while true; do
+    TERMINATING_PODS=$(kubectl get pods -n "${NAMESPACE}" | grep Terminating)
+    if [ -z "${TERMINATING_PODS}" ]; then
+      log "No pods in Terminating state"
+      exit 0
+    else
+      log "Terminating pods found: ${TERMINATING_PODS}"
+    fi
+    CURRENT_TIME=$(date +%s)
+    ELAPSED_TIME=$((CURRENT_TIME - START_TIME))
+    if [ $ELAPSED_TIME -ge $TIMEOUT ]; then
+      log "ERROR" "Timeout reached. Pods are still in Terminating state."
+      exit 1
+    fi
+    sleep ${INTERVAL}
+  done
+else
+  log "No need to scale down"
+fi
