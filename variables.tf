926b02d50c8c4a1bbdf1a932184c56eb5bf361e1
vkavalenka80 <vkavalenka@atlassian.com>
2025-09-11 12:32:56 +0200

    Bump default osquery version (#456)

--- variables.tf
+++ variables.tf
@@ -1353,7 +1353,7 @@ variable "osquery_env" {
 variable "osquery_version" {
   description = "Osquery version"
   type        = string
-  default     = "5.7.0"
+  default     = "5.14.1"
 }
 
 variable "osquery_fleet_enrollment_host" {
